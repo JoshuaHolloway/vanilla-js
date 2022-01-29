7df58792e77290727bcb07c7d35a02d024349bfa
JoshuaHolloway <jhollow6@asu.edu>
2022-01-29 00:47:07 -0600

    blur

--- demos/image-processing/opencv/lib/js/editor.js
+++ demos/image-processing/opencv/lib/js/editor.js
@@ -3,9 +3,8 @@ const executeCodeBtn = document.querySelector('.editor__run');
 const resetCodeBtn = document.querySelector('.editor__reset');
 
 // Setup Ace
-let codeEditor = ace.edit("editorCode");
-let defaultCode = 
-`// Grab the data from the opened image file
+let codeEditor = ace.edit('editorCode');
+let defaultCode = `// Grab the data from the opened image file
 let mat = cv.imread(imgElement);
 
 console.log('image width: '   + mat.cols);
@@ -25,58 +24,62 @@ cv.split(mat, rgbaPlanes);
 // Get R channel
 let R = rgbaPlanes.get(0);
 
-// Display to canvas element with id="canvasOutput"
-cv.imshow('canvasOutput', R);
 
-// Merge all channels
-//cv.merge(rgbaPlanes, src);
+let src = R;
+let dst = new cv.Mat();
+let ksize = new cv.Size(23, 23);
+// You can try more different parameters
+cv.GaussianBlur(src, dst, ksize, 0, 0, cv.BORDER_DEFAULT);
+cv.imshow('canvasOutput', dst);
+src.delete(); dst.delete();
 
 
 // Clean up 
 mat.delete(); 
 rgbaPlanes.delete(); 
 R.delete();
+
 `;
 
 let editorLib = {
-    init() {
-        // Configure Ace
+  init() {
+    // Configure Ace
 
-        // Theme
-        //codeEditor.setTheme("ace/theme/dreamweaver");
+    // Theme
+    //codeEditor.setTheme("ace/theme/dreamweaver");
 
-        // Set language
-        codeEditor.session.setMode("ace/mode/javascript");
+    // Set language
+    codeEditor.session.setMode('ace/mode/javascript');
 
-        // Set Options
-        // codeEditor.setOptions({
-        //     fontFamily: 'Inconsolata',
-        //     fontSize: '12pt',
-        //     enableBasicAutocompletion: true,
-        //     enableLiveAutocompletion: true,
-        // });
+    // Set Options
+    // codeEditor.setOptions({
+    //     fontFamily: 'Inconsolata',
+    //     fontSize: '12pt',
+    //     enableBasicAutocompletion: true,
+    //     enableLiveAutocompletion: true,
+    // });
 
-        // Set Default Code
-        codeEditor.setValue(defaultCode);
-    }
-}
+    // Set Default Code
+    codeEditor.setValue(defaultCode);
+  },
+};
 
 // Events
 executeCodeBtn.addEventListener('click', () => {
-    // Get input from the code editor
-    const userCode = codeEditor.getValue();
+  // Get input from the code editor
+  const userCode = codeEditor.getValue();
 
-    // Run the user code
-    try {
-        new Function(userCode)();
-    } catch (err) {
-        console.error(err);
-    }
+  // Run the user code
+  try {
+    new Function(userCode)();
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 resetCodeBtn.addEventListener('click', () => {
-    // Clear ace editor
-    codeEditor.setValue(defaultCode);
-})
+  // Clear ace editor
+  codeEditor.setValue(defaultCode);
+});
 
 editorLib.init();
